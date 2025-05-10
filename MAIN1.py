e876c7f05235179c76b3d505ba2648a2362cda21
PIYUSHKUMAR95-project <piyush901kr@gmail.com>
2025-05-10 11:49:01 +0530

    Add files via upload

---
+++ MAIN1.py
@@ -0,0 +1,23 @@
+import streamlit as st
+
+st.title("Welcome to my hard earned website")
+st.header("Python")
+st.subheader("Java")
+st.markdown("I love python")
+st.code("""for i in range(1,5,1):
+                print("Hello")
+        """)
+
+name = st.text_input("Enter your name")
+fname = st.text_input("Enter your Father's name")
+adr = st.text_input("Enter your Text: ")
+classdata = st.selectbox("Enter your Class :",(1,2,3,4,5,6))
+
+button = st.button("Done")
+if button :
+    st.markdown(f"""
+    Name: {name}
+    Father Name : {fname}
+    address : {adr}
+    class : {classdata}
+""")
